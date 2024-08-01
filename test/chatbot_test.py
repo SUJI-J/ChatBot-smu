ac6caa033a7889cc615a1c6fdc17450f63649f9a
SUJI-J <suzy852@naver.com>
2024-08-01 14:15:46 +0900

    Feat: 답변 검색 테스트

---
+++ test/chatbot_test.py
@@ -0,0 +1,58 @@
+from utils.Preprocess import Preprocess
+from models.intent.IntentModel import IntentModel
+import pymysql
+from config.DatabaseConfig import *
+
+p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin',
+               userdic='../utils/user_dic.tsv')
+
+db = pymysql.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    passwd=DB_PASSWORD,
+    db=DB_NAME,
+    port=DB_PORT,
+    charset='utf8'
+)
+db.connect()
+
+# 원문
+# query = "오전에 탕수육 10개 주문합니다"
+# query = "화자의 질문 의도를 파악합니다."
+# query = "안녕하세요"
+query = "스마트정보통신공학과 학과사무실 번호 알려줘"
+
+# 의도 파악
+from models.intent.IntentModel import IntentModel
+
+intent = IntentModel(model_name='../models/intent/intent_model.h5', proprocess=p)
+predict = intent.predict_class(query)
+intent_name = intent.labels[predict]
+
+# 개체명 인식
+from models.ner.NerModel import NerModel
+
+ner = NerModel(model_name='../models/ner/ner_model.h5', proprocess=p)
+predicts = ner.predict(query)
+ner_tags = ner.predict_tags(query)
+
+print("질문 : ", query)
+print("=" * 100)
+print("의도 파악 : ", intent_name)
+print("개체명 인식 : ", predicts)
+print("답변 검색에 필요한 NER 태그 : ", ner_tags)
+print("=" * 100)
+
+# 답변 검색
+from utils.FindAnswer import FindAnswer
+
+try:
+    f = FindAnswer(db)
+    answer_text, answer_image = f.search(intent_name, ner_tags)
+    answer = f.tag_to_word(predicts, answer_text)
+except:
+    answer = "죄송해요 무슨 말인지 모르겠어요"
+
+print("답변 : ", answer)
+
+db.close()  # 디비 연결 끊음
